3b6d27eccbfed54a808ac0d6998c58bad43227bf
German Siliprandi <german.siliprandi@gmail.com>
2022-05-20 17:30:37 -0300

    Mascotamania v1.0.0

--- js/main.js
+++ js/main.js
@@ -4,34 +4,45 @@ var form = document.getElementById("my-form");
 
 async function handleSubmit(event) {
 	event.preventDefault();
+	const inputNombre = document.getElementById("inputNombre").value;
+	const inputEmail = document.getElementById("inputEmail").value;
+	const mensaje = document.getElementById("message").value;
+
 	var status = document.getElementById("my-form-status");
-	var data = new FormData(event.target);
-	fetch(event.target.action, {
-		method: form.method,
-		body: data,
-		headers: {
-			Accept: "application/json",
-		},
-	})
-		.then((response) => {
-			if (response.ok) {
-				status.innerHTML = "¡Gracias por su mensaje!";
-				form.reset();
-			} else {
-				response.json().then((data) => {
-					if (Object.hasOwn(data, "errors")) {
-						status.innerHTML = data["errors"]
-							.map((error) => error["message"])
-							.join(", ");
-					} else {
-						status.innerHTML =
-							"Oops! There was a problem submitting your form";
-					}
-				});
-			}
+	if (inputEmail != "" && inputEmail != "" && mensaje != "") {
+		var data = new FormData(event.target);
+
+		fetch(event.target.action, {
+			method: form.method,
+			body: data,
+			headers: {
+				Accept: "application/json",
+			},
 		})
-		.catch((error) => {
-			status.innerHTML = "Oops! There was a problem submitting your form";
-		});
+			.then((response) => {
+				if (response.ok) {
+					status.innerHTML = "¡Gracias por su mensaje!";
+					form.reset();
+				} else {
+					response.json().then((data) => {
+						if (Object.hasOwn(data, "errors")) {
+							status.innerHTML = data["errors"]
+								.map((error) => error["message"])
+								.join(", ");
+						} else {
+							status.innerHTML =
+								"Oops! There was a problem submitting your form";
+						}
+					});
+				}
+			})
+			.catch((error) => {
+				status.innerHTML =
+					"Oops! There was a problem submitting your form";
+			});
+	} else {
+		status.innerHTML =
+			"Los campos Nombre, Email y mensaje son obligatorios";
+	}
 }
 form.addEventListener("submit", handleSubmit);
